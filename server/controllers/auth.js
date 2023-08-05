5ddaed84e93e5545dc2505410d57db61cc6d69de
DUPIN <maximedupin1992@gmail.com>
2023-08-05 12:30:55 +0200

    pm2

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,58 +1,61 @@
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
 
 // REGISTER ET LOGIN
 
-export const register = async (req, res)=>{
-    try {
-        const {
-            firstName,
-            lastName,
-            email,
-            password,
-            picturePath,
-            friends,
-            location,
-            occupation
-        }= req.body;
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
 
-        const salt = await bcrypt.genSalt();
-        const passwordHash = await bcrypt.hash(password, salt)
-        const newUser = new User({
-            firstName,
-            lastName,
-            email,
-            password: passwordHash,
-            picturePath,
-            friends,
-            location,
-            occupation,
-            viewedProfile:Math.floor(Math.random()* 100000),
-            impressions:Math.floor(Math.random()* 100000)
-        })
-        const savedUser = await newUser.save();
-        res.status(201).json(savedUser)
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 100000),
+      impressions: Math.floor(Math.random() * 100000),
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 
-    } catch(err){
-        res.status(500).json({ error: err.message})
-    }
-}
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-export const login = async (req, res)=>{
-    try{
-        const {email, password}= req.body;
+    const user = await User.findOne({ email: email });
+    if (!user)
+      return res.status(400).json({ msg: " L'utilisateur n'existe pas" });
 
-        const user = await User.findOne({email: email});
-        if (!user) return res.status(400).json( {msg: " L'utilisateur n'existe pas"})
+    const Boolcompare = await bcrypt.compare(password, user.password);
+    if (!Boolcompare)
+      return res
+        .status(400)
+        .json({ msg: ' Le mot de passe ne correspond pas !' });
 
-        const Boolcompare = await bcrypt.compare(password, user.password);
-        if(!Boolcompare) return res.status(400).json( {msg: " Le mot de passe ne correspond pas !"})
-
-        const token =jwt.sign({id: user._id}, process.env.JWT_SECRET);
-        delete user.password;
-        res.status(200).json( {token, user});
-    } catch(err) {
-        res.status(500).json({error: err.message})
-    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ token, user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
--- server/models/User.js
+++ server/models/User.js
@@ -1,46 +1,47 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema(
-    {
-        firstName:{
-            type:String,
-            required: true,
-            min:2,
-            max:45
-        },
-        lastName:{
-            type:String,
-            required: true,
-            min:2,
-            max:50
-        },
-        email:{
-            type:String,
-            required: true,
-            min:2,
-            max:50,
-            unique: true
-        },
-        password:{
-            type:String,
-            required: true,
-            min:5,
-        },
-        friends: {
-            type:Array,
-            default:[]
-        },
-        picturePath:{
-            type:String,
-            default:""
-        },
-        location: String,
-        occupation:String,
-        viewedProfile: Number,
-        impressions: Number,  
-    }, {timestamps:true}
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 45,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    picturePath: {
+      type: String,
+      default: '',
+    },
+    location: String,
+    occupation: String,
+    viewedProfile: Number,
+    impressions: Number,
+  },
+  { timestamps: true },
 );
 
-const User = mongoose.model("User", UserSchema)
+const User = mongoose.model('User', UserSchema);
 
 export default User;
